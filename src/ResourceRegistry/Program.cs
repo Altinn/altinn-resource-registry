76342889e16d73134562b265ddd3a80659cb32ff
Rune T. Larsen <rune@hjemmekino.no>
2022-10-06 13:52:29 +0200

    Fixing providers

--- src/ResourceRegistry/Program.cs
+++ src/ResourceRegistry/Program.cs
@@ -22,7 +22,7 @@ string applicationInsightsConnectionString = string.Empty;
 var builder = WebApplication.CreateBuilder(args);
 ConfigureSetupLogging();
 
-await ConnectToKeyVaultAndSetApplicationInsights(builder.Configuration);
+await SetConfigurationProviders(builder.Configuration);
 
 ConfigureLogging(builder.Logging);
 
@@ -84,6 +84,33 @@ void Configure(IConfiguration config)
     });
 
     app.MapControllers();
+}
+
+async Task SetConfigurationProviders(ConfigurationManager config)
+{
+    string basePath = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
+
+    logger.LogInformation($"Program // Loading Configuration from basePath={basePath}");
+
+    config.SetBasePath(basePath);
+    string configJsonFile1 = $"{basePath}/altinn-appsettings/altinn-dbsettings-secret.json";
+    string configJsonFile2 = $"{Directory.GetCurrentDirectory()}/appsettings.json";
+
+    if (basePath == "/")
+    {
+        configJsonFile2 = "/app/appsettings.json";
+    }
+
+    logger.LogInformation($"Loading configuration file: '{configJsonFile1}'");
+    config.AddJsonFile(configJsonFile1, optional: true, reloadOnChange: true);
+
+    logger.LogInformation($"Loading configuration file2: '{configJsonFile2}'");
+    config.AddJsonFile(configJsonFile2, optional: false, reloadOnChange: true);
+
+    config.AddEnvironmentVariables();
+    config.AddCommandLine(args);
+
+    await ConnectToKeyVaultAndSetApplicationInsights(config);
 }
 
 void ConfigurePostgreSql()

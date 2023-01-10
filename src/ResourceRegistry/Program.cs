0cf182c1f7794b0b9d1fc9211288520cdaa0937f
Sondre Bråten <sondre.braten@avanade.com>
2023-01-10 14:39:02 +0100

    Removed commented-out code.

--- src/ResourceRegistry/Program.cs
+++ src/ResourceRegistry/Program.cs
@@ -54,7 +54,6 @@ app.Run();
 
 void ConfigureServices(IServiceCollection services, IConfiguration config)
 {
-    //PlatformSettings platformSettings = config.GetSection("PlatformSettings").Get<PlatformSettings>();
     services.AddControllers().AddJsonOptions(options =>
     {
         options.JsonSerializerOptions.WriteIndented = true;

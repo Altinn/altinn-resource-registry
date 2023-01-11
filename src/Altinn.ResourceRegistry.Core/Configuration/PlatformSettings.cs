2ce11df72ab672916166ead372bdb65d059c36ed
Remi Løvoll <remi.lovoll@avanade.com>
2023-01-11 14:04:57 +0100

    More cleanup

--- src/Altinn.ResourceRegistry.Core/Configuration/PlatformSettings.cs
+++ src/Altinn.ResourceRegistry.Core/Configuration/PlatformSettings.cs
@@ -24,23 +24,8 @@ namespace Altinn.ResourceRegistry.Core.Configuration
         public string AccessTokenApp { get; set; } = "ResourceRegister";
 
         /// <summary>
-        /// The headder value to use for ACcessToken
+        /// The headder value to use for AccessToken
         /// </summary>
         public string AccessTokenHeaderId { get; set; } = "PlatformAccessToken";
-
-        /// <summary>
-        /// Open Id Connect Well known endpoint
-        /// </summary>
-        public string? OpenIdWellKnownEndpoint { get; set; }
-
-        /// <summary>
-        /// Name of the cookie for where JWT is stored
-        /// </summary>
-        public string? JwtCookieName { get; set; }
-
-        /// <summary>
-        /// Endpoint for authentication
-        /// </summary>
-        public string? ApiAuthenticationEndpoint { get; set; }
     }
 }

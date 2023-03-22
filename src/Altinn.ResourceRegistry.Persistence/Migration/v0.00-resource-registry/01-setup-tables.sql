e6a001ced527029c2bfed26ab95fc331dbf0ad1d
Remi Løvoll <remi.lovoll@avanade.com>
2023-03-22 09:01:47 +0100

    Changed timestamp data type from timetz to timestamptz to add date info not only clock info. Nobody is interested in a resource created 14;38 but not knowing witch date it was. Will delete the notification that the script is run so it wil actualy run the next time code is deployed.
    Also changed the resource type to reflect the actual types in the enum in code. Also fixed this directly in AT21-AT24.

--- src/Altinn.ResourceRegistry.Persistence/Migration/v0.00-resource-registry/01-setup-tables.sql
+++ src/Altinn.ResourceRegistry.Persistence/Migration/v0.00-resource-registry/01-setup-tables.sql
@@ -1,12 +1,14 @@
 -- Schema: resourceregistry
 CREATE SCHEMA IF NOT EXISTS resourceregistry;
 
+DROP TABLE IF EXISTS resourceregistry.resources
+
 -- Table: resourceregistry.resources
-CREATE TABLE IF NOT EXISTS resourceregistry.resources
+CREATE TABLE  resourceregistry.resources
 (
     identifier text COLLATE pg_catalog."default" NOT NULL,
-    created time with time zone NOT NULL,
-    modified time with time zone,
+    created timestamp with time zone NOT NULL,
+    modified timestamp with time zone,
     serviceresourcejson jsonb,
     CONSTRAINT resourceregistry_pkey PRIMARY KEY (identifier)
 )
@@ -14,7 +16,7 @@ TABLESPACE pg_default;
 
 -- Enum: resourceregistry.resourcetype
 DO $$ BEGIN
-    CREATE TYPE resourceregistry.resourcetype AS ENUM ('default', 'systemresource', 'altinn2', 'altinn3', 'apischema', 'api');
+    CREATE TYPE resourceregistry.resourcetype AS ENUM ('default', 'systemresource', 'maskinportenschema');
 EXCEPTION
     WHEN duplicate_object THEN null;
 END $$;

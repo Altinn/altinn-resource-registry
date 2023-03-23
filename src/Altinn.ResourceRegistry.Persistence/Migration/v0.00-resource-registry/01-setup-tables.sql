2fb15dbddbf4c7ffa4ed7c805a5820b2793ffec2
Remi Løvoll <remi.lovoll@avanade.com>
2023-03-23 10:28:16 +0100

    Fixed dependencies and missing semicolon

--- src/Altinn.ResourceRegistry.Persistence/Migration/v0.00-resource-registry/01-setup-tables.sql
+++ src/Altinn.ResourceRegistry.Persistence/Migration/v0.00-resource-registry/01-setup-tables.sql
@@ -1,7 +1,12 @@
 -- Schema: resourceregistry
 CREATE SCHEMA IF NOT EXISTS resourceregistry;
 
-DROP TABLE IF EXISTS resourceregistry.resources
+DROP FUNCTION IF EXISTS resourceregistry.create_resource(text, jsonb);
+DROP FUNCTION IF EXISTS resourceregistry.delete_resource(text);
+DROP FUNCTION IF EXISTS resourceregistry.get_resource(character varying);
+DROP FUNCTION IF EXISTS resourceregistry.search_for_resource(text, text, text, resourceregistry.resourcetype, text, boolean);
+DROP FUNCTION IF EXISTS resourceregistry.update_resource(text, jsonb);
+DROP TABLE IF EXISTS resourceregistry.resources;
 
 -- Table: resourceregistry.resources
 CREATE TABLE  resourceregistry.resources

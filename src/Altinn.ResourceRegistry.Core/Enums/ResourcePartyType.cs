d6110d2572cb8e47a032eaba6cccdbf39395af91
Rune Tømmerås Larsen <rune@hjemmekino.no>
2023-09-21 11:15:59 +0200

    Fixed enum serialization (#251)

--- src/Altinn.ResourceRegistry.Core/Enums/ResourcePartyType.cs
+++ src/Altinn.ResourceRegistry.Core/Enums/ResourcePartyType.cs
@@ -1,10 +1,12 @@
 ﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace Altinn.ResourceRegistry.Core.Enums
 {
     /// <summary>
     /// Defines the type of party that a resource is targeting
     /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum ResourcePartyType
     {
         [EnumMember(Value = "PrivatePerson")]

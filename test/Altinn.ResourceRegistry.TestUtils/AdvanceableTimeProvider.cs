dc46c4372016e1f14098f34340ee85fd89861d9c
Aleksander Heintz <alxandr@alxandr.me>
2024-01-31 13:43:06 +0100

    chore: add advanceable time provider to test utils

---
+++ test/Altinn.ResourceRegistry.TestUtils/AdvanceableTimeProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace Altinn.ResourceRegistry.TestUtils;
+
+public class AdvanceableTimeProvider : TimeProvider
+{
+    private readonly TimeProvider _inner = TimeProvider.System;
+    
+    private TimeSpan _offset;
+
+    public override DateTimeOffset GetUtcNow()
+        => _inner.GetUtcNow() + _offset;
+
+    public override long GetTimestamp()
+        => _inner.GetTimestamp() + _offset.Ticks;
+
+    public void Advance(TimeSpan offset)
+        => _offset += offset;
+}

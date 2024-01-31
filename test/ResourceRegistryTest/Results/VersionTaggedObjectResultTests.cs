5f8538f9b942152657261997c376cbcc5f07fe27
Aleksander Heintz <alxandr@alxandr.me>
2024-01-31 15:06:11 +0100

    feat: VersionedTaggedObjectResult

---
+++ test/ResourceRegistryTest/Results/VersionTaggedObjectResultTests.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Options;
+using Altinn.ResourceRegistry.Tests.Utils;
+using System.Threading.Tasks;
+using Altinn.ResourceRegistry.Results;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using FluentAssertions;
+using Xunit;
+using Altinn.ResourceRegistry.Core.Models;
+using System;
+
+namespace Altinn.ResourceRegistry.Tests.Results;
+
+public class VersionTaggedObjectResultTests
+{
+    [Fact]
+    public async Task SetsStatusCode()
+    {
+        var result = new VersionedTaggedObjectResult("value", null, null)
+        {
+            Formatters = [new NoOpOutputFormatter()],
+        };
+
+        var context = GetActionContext();
+        await result.ExecuteResultAsync(context);
+
+        context.HttpContext.Response.StatusCode.Should().Be(StatusCodes.Status200OK);
+    }
+
+    [Fact]
+    public async Task SetsETagHeader()
+    {
+        var result = new VersionedTaggedObjectResult("value", "tag", null)
+        {
+            Formatters = [new NoOpOutputFormatter()],
+        };
+
+        var context = GetActionContext();
+        await result.ExecuteResultAsync(context);
+
+        context.HttpContext.Response.Headers.ETag.Should().ContainSingle().Which.Should().Be("tag");
+    }
+
+    [Fact]
+    public async Task SetsLastModifiedHeader()
+    {
+        var result = new VersionedTaggedObjectResult("value", null, new HttpDateTimeHeaderValue(new DateTimeOffset(2021, 01, 01, 01, 01, 01, TimeSpan.Zero)))
+        {
+            Formatters = [new NoOpOutputFormatter()],
+        };
+
+        var context = GetActionContext();
+        await result.ExecuteResultAsync(context);
+
+        context.HttpContext.Response.Headers.LastModified.Should().ContainSingle().Which.Should().Be("Fri, 01 Jan 2021 01:01:01 GMT");
+    }
+
+    private static IServiceCollection CreateServices()
+    {
+        var services = new ServiceCollection();
+        var options = Options.Create(new MvcOptions());
+        services.AddSingleton<ILoggerFactory>(NullLoggerFactory.Instance);
+        services.AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
+        services.AddSingleton<IActionResultExecutor<ObjectResult>>(new ObjectResultExecutor(
+            new DefaultOutputFormatterSelector(options, NullLoggerFactory.Instance),
+            new TestHttpResponseStreamWriterFactory(),
+            NullLoggerFactory.Instance,
+            options));
+        return services;
+    }
+
+    private static HttpContext GetHttpContext()
+    {
+        var services = CreateServices();
+
+        var httpContext = new DefaultHttpContext();
+        httpContext.RequestServices = services.BuildServiceProvider();
+
+        return httpContext;
+    }
+
+    private static ActionContext GetActionContext()
+    {
+        var httpContext = GetHttpContext();
+        var routeData = new RouteData();
+        var actionDescriptor = new ActionDescriptor();
+        return new ActionContext(httpContext, routeData, actionDescriptor);
+    }
+
+    private class NoOpOutputFormatter : IOutputFormatter
+    {
+        public bool CanWriteResult(OutputFormatterCanWriteContext context)
+        {
+            return true;
+        }
+
+        public Task WriteAsync(OutputFormatterWriteContext context)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ test/ResourceRegistryTest/Utils/TestHttpResponseStreamWriterFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.WebUtilities;
+using System.IO;
+using System.Text;
+
+namespace Altinn.ResourceRegistry.Tests.Utils;
+
+public class TestHttpResponseStreamWriterFactory : IHttpResponseStreamWriterFactory
+{
+    public const int DefaultBufferSize = 16 * 1024;
+
+    public TextWriter CreateWriter(Stream stream, Encoding encoding)
+    {
+        return new HttpResponseStreamWriter(stream, encoding, DefaultBufferSize);
+    }
+}

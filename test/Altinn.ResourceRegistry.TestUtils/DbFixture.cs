28b39b7ba8533f90eb486268812364bdef730496
Aleksander Heintz <alxandr@alxandr.me>
2024-01-31 16:27:19 +0100

    chore: update db test fixture (#346)

--- test/Altinn.ResourceRegistry.TestUtils/DbFixture.cs
+++ test/Altinn.ResourceRegistry.TestUtils/DbFixture.cs
@@ -1,5 +1,4 @@
 ﻿using Altinn.ResourceRegistry.Persistence.Configuration;
-using FluentAssertions.Common;
 using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
 using Testcontainers.PostgreSql;
@@ -7,51 +6,18 @@ using Yuniql.Core;
 
 namespace Altinn.ResourceRegistry.TestUtils;
 
-public class DbFixture : IAsyncLifetime
+public class DbFixture 
+    : IAsyncLifetime
 {
-    private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
-        .WithImage("docker.io/postgres:16.1-alpine")
-        .WithCleanUp(true)
-        .Build();
-
-    string? _connectionString;
-    NpgsqlDataSource? _db;
+    Singleton.Ref<Inner>? _inner;
 
     public async Task InitializeAsync()
     {
-        await _dbContainer.StartAsync();
-        _connectionString = _dbContainer.GetConnectionString();
-        _db = NpgsqlDataSource.Create(_connectionString);
+        _inner = await Singleton.Get<Inner>();
     }
 
-    public async Task<OwnedDb> CreateDbAsync()
-    {
-        var dbName = $"test_{Guid.NewGuid():N}";
-
-        await using var cmd = _db!.CreateCommand(/*strpsql*/$"CREATE DATABASE {dbName};");
-
-        await cmd.ExecuteNonQueryAsync();
-
-        var connectionStringBuilder = new NpgsqlConnectionStringBuilder(_connectionString) { Database = dbName };
-        var connectionString = connectionStringBuilder.ToString();
-
-        var configuration = new Yuniql.AspNetCore.Configuration();
-        configuration.Platform = SUPPORTED_DATABASES.POSTGRESQL;
-        configuration.Workspace = Path.Combine(FindWorkspace(), "src", "Altinn.ResourceRegistry.Persistence", "Migration");
-        configuration.ConnectionString = connectionString;
-        configuration.IsAutoCreateDatabase = false;
-        configuration.Environment = "integrationtest";
-
-        var traceService = TraceService.Instance;
-        var dataService = new Yuniql.PostgreSql.PostgreSqlDataService(traceService);
-        var bulkImportService = new Yuniql.PostgreSql.PostgreSqlBulkImportService(traceService);
-        var migrationServiceFactory = new MigrationServiceFactory(traceService);
-        var migrationService = migrationServiceFactory.Create(dataService, bulkImportService);
-        ConfigurationHelper.Initialize(configuration);
-        migrationService.Run();
-
-        return new OwnedDb(connectionString, dbName, _db);
-    }
+    public Task<OwnedDb> CreateDbAsync()
+        => _inner!.Value.CreateDbAsync();
 
     public async Task<IAsyncDisposable> ConfigureServicesAsync(IServiceCollection services)
     {
@@ -63,28 +29,90 @@ public class DbFixture : IAsyncLifetime
 
     public async Task DisposeAsync()
     {
-        if (_db is { } db)
+        if (_inner is { } inner)
         {
-            await db.DisposeAsync();
+            await inner.DisposeAsync();
         }
-        
-        await _dbContainer.DisposeAsync();
     }
 
-    static string FindWorkspace()
+    private class Inner : IAsyncLifetime
     {
-        var dir = Environment.CurrentDirectory;
-        while (dir != null)
-        {
-            if (Directory.Exists(Path.Combine(dir, ".git")))
-            {
-                return dir;
-            }
+        private int _dbCounter = 0;
+        private readonly AsyncLock _dbLock = new();
+        private readonly PostgreSqlContainer _dbContainer = new PostgreSqlBuilder()
+            .WithImage("docker.io/postgres:16.1-alpine")
+            .WithCleanUp(true)
+            .Build();
 
-            dir = Directory.GetParent(dir)?.FullName;
+        string? _connectionString;
+        NpgsqlDataSource? _db;
+
+        public async Task InitializeAsync()
+        {
+            await _dbContainer.StartAsync();
+            _connectionString = _dbContainer.GetConnectionString();
+            _db = NpgsqlDataSource.Create(_connectionString);
         }
 
-        throw new InvalidOperationException("Workspace directory not found");
+        public async Task<OwnedDb> CreateDbAsync()
+        {
+            var dbName = $"test_{Interlocked.Increment(ref _dbCounter)}";
+
+            // only create 1 db at once
+            using var guard = await _dbLock.Acquire();
+
+            await using var cmd = _db!.CreateCommand(/*strpsql*/$"CREATE DATABASE {dbName};");
+
+            await cmd.ExecuteNonQueryAsync();
+
+            var connectionStringBuilder = new NpgsqlConnectionStringBuilder(_connectionString) { Database = dbName, IncludeErrorDetail = true };
+            var connectionString = connectionStringBuilder.ToString();
+
+            var configuration = new Yuniql.AspNetCore.Configuration();
+            configuration.Platform = SUPPORTED_DATABASES.POSTGRESQL;
+            configuration.Workspace = Path.Combine(FindWorkspace(), "src", "Altinn.ResourceRegistry.Persistence", "Migration");
+            configuration.ConnectionString = connectionString;
+            configuration.IsAutoCreateDatabase = false;
+            configuration.Environment = "integrationtest";
+
+            var traceService = TraceService.Instance;
+            var dataService = new Yuniql.PostgreSql.PostgreSqlDataService(traceService);
+            var bulkImportService = new Yuniql.PostgreSql.PostgreSqlBulkImportService(traceService);
+            var migrationServiceFactory = new MigrationServiceFactory(traceService);
+            var migrationService = migrationServiceFactory.Create(dataService, bulkImportService);
+            ConfigurationHelper.Initialize(configuration);
+            migrationService.Run();
+
+            return new OwnedDb(connectionString, dbName, _db);
+        }
+
+        public async Task DisposeAsync()
+        {
+            if (_db is { } db)
+            {
+                await db.DisposeAsync();
+            }
+
+            await _dbContainer.DisposeAsync();
+
+            _dbLock.Dispose();
+        }
+
+        static string FindWorkspace()
+        {
+            var dir = Environment.CurrentDirectory;
+            while (dir != null)
+            {
+                if (Directory.Exists(Path.Combine(dir, ".git")))
+                {
+                    return dir;
+                }
+
+                dir = Directory.GetParent(dir)?.FullName;
+            }
+
+            throw new InvalidOperationException("Workspace directory not found");
+        }
     }
 
     public sealed class OwnedDb : IAsyncDisposable
@@ -177,4 +205,3 @@ public class DbFixture : IAsyncLifetime
         }
     }
 }
-

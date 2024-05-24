9399bf7eee008d470513eece16deb3c287137c33
Rune T. Larsen <rune@hjemmekino.no>
2024-05-24 12:51:24 +0200

    Updates url

--- util/ResourceSubjectsLoader/Program.cs
+++ util/ResourceSubjectsLoader/Program.cs
@@ -5,7 +5,7 @@ using ResourceSubjectsLoader.Enums;
 
 Console.WriteLine("Hello, World!");
 
-string at22base = "https://platform.at22.altinn.cloud/resourceregistry/api/v1/";
+string at22base = "https://platform.altinn.no/resourceregistry/api/v1/";
 
 HttpClient _httpClient = new HttpClient();
 _httpClient.BaseAddress = new Uri(at22base);    

d275464932a8c9c3f43d8b574dde9a279309fe09
Aleksander Heintz <alxandr@alxandr.me>
2024-06-05 11:00:16 +0200

    fix: less logging

--- src/Altinn.ResourceRegistry/Program.cs
+++ src/Altinn.ResourceRegistry/Program.cs
@@ -214,6 +214,11 @@ void Configure(IConfiguration config)
     // TODO: Remove (or move to proper middleware) once URL generation is fixed
     app.Use((ctx, next) =>
     {
+        if (ctx.Request.Path.StartsWithSegments("/health"))
+        {
+            return next();
+        }
+
         var loggerFactory = ctx.RequestServices.GetRequiredService<ILoggerFactory>();
         var logger = loggerFactory.CreateLogger(REQUESTINFO_LOGGER_NAME);
         var options = ctx.RequestServices.GetRequiredService<IOptions<ForwardedHeadersOptions>>().Value;

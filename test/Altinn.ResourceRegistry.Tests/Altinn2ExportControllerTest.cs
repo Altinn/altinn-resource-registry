a9119ab850713555f72d7fcc7f7a972c6c5be92e
Rune T. Larsen <rune@hjemmekino.no>
2024-07-10 14:36:38 +0200

    Merge fixes

--- test/Altinn.ResourceRegistry.Tests/Altinn2ExportControllerTest.cs
+++ test/Altinn.ResourceRegistry.Tests/Altinn2ExportControllerTest.cs
@@ -1,4 +1,9 @@
+using Altinn.ResourceRegistry.Core.Models.Altinn2;
+using Altinn.ResourceRegistry.Tests.Utils;
 using Altinn.ResourceRegistry.TestUtils;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace Altinn.ResourceRegistry.Tests
 {
@@ -41,7 +46,7 @@ namespace Altinn.ResourceRegistry.Tests
         [Fact]
         public async Task DelegationCount()
         {
-            HttpClient client = SetupUtil.GetTestClient(_factory);
+            HttpClient client = CreateClient();
             string token = PrincipalUtil.GetAccessToken("studio.designer");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             string requestUri = "resourceregistry/api/v1/altinn2export/delegationcount?serviceCode=4485&serviceEditionCode=2021";
@@ -68,7 +73,7 @@ namespace Altinn.ResourceRegistry.Tests
         [Fact]
         public async Task DelegationCount_WrongConsumer()
         {
-            HttpClient client = SetupUtil.GetTestClient(_factory);
+            HttpClient client = CreateClient();
             string token = PrincipalUtil.GetAccessToken("studi2o.designer");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             string requestUri = "resourceregistry/api/v1/altinn2export/delegationcount?serviceCode=4485&serviceEditionCode=2021";
@@ -88,7 +93,7 @@ namespace Altinn.ResourceRegistry.Tests
         [Fact]
         public async Task DelegationCount_WithoutToken()
         {
-            HttpClient client = SetupUtil.GetTestClient(_factory);
+            HttpClient client = CreateClient();
             string requestUri = "resourceregistry/api/v1/altinn2export/delegationcount?serviceCode=4485&serviceEditionCode=2021";
 
             HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, requestUri)
@@ -106,7 +111,7 @@ namespace Altinn.ResourceRegistry.Tests
         [Fact]
         public async Task Trigger_Batch_Unauthorized()
         {
-            HttpClient client = SetupUtil.GetTestClient(_factory);
+            HttpClient client = CreateClient();
             string requestUri = "resourceregistry/api/v1/altinn2export/exportdelegations";
 
             ExportDelegationsRequestBE exportDelegationsRequestBE = new ExportDelegationsRequestBE()
@@ -129,7 +134,7 @@ namespace Altinn.ResourceRegistry.Tests
         [Fact]
         public async Task Trigger_Batch_MissingResource()
         {
-            HttpClient client = SetupUtil.GetTestClient(_factory);
+            HttpClient client = CreateClient();
             string token = PrincipalUtil.GetAccessToken("studio.designer");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             string requestUri = "resourceregistry/api/v1/altinn2export/exportdelegations";
@@ -160,7 +165,7 @@ namespace Altinn.ResourceRegistry.Tests
         [Fact]
         public async Task Trigger_Batch_Ok()
         {
-            HttpClient client = SetupUtil.GetTestClient(_factory);
+            HttpClient client = CreateClient();
             string token = PrincipalUtil.GetAccessToken("studio.designer");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             string requestUri = "resourceregistry/api/v1/altinn2export/exportdelegations";
@@ -187,7 +192,7 @@ namespace Altinn.ResourceRegistry.Tests
         [Fact]
         public async Task Trigger_Batch_NoMatchingORg()
         {
-            HttpClient client = SetupUtil.GetTestClient(_factory);
+            HttpClient client = CreateClient();
             string token = PrincipalUtil.GetAccessToken("studio.designer");
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             string requestUri = "resourceregistry/api/v1/altinn2export/exportdelegations";

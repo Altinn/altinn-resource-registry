0afba32cea8eb7dc772b18907034a02e810973bb
Aleksander Heintz <alxandr@alxandr.me>
2024-07-11 10:56:07 +0200

    chore: enable PII logging in development

--- src/Altinn.ResourceRegistry/Program.cs
+++ src/Altinn.ResourceRegistry/Program.cs
@@ -1,4 +1,5 @@
 using Altinn.ResourceRegistry;
+using Microsoft.IdentityModel.Logging;
 
 WebApplication app = ResourceRegistryHost.Create(args);
 
@@ -6,6 +7,10 @@ app.AddDefaultAltinnMiddleware(errorHandlingPath: "/resourceregistry/api/v1/erro
 
 if (app.Environment.IsDevelopment())
 {
+    // Enable higher level of detail in exceptions related to JWT validation
+    IdentityModelEventSource.ShowPII = true;
+
+    // Enable Swagger
     app.UseSwagger();
     app.UseSwaggerUI();
 }

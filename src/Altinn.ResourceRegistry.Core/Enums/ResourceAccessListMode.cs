486768f5f655ae6dd4b082961391fea4feab3aab
Rune T. Larsen <rune@hjemmekino.no>
2024-09-05 15:52:44 +0200

    Changed to accessList mode

---
+++ src/Altinn.ResourceRegistry.Core/Enums/ResourceAccessListMode.cs
@@ -0,0 +1,12 @@
+﻿namespace Altinn.ResourceRegistry.Core.Enums
+{
+    /// <summary>
+    /// Enum representation of the different types of ResourceAccessListModes supported by the resource registry
+    /// </summary>
+    public enum ResourceAccessListMode
+    {
+        Disabled = 0,
+
+        Enabled = 1,
+     }
+}

1e09eeed1751c2219341386665bb1cdcc3e637f2
Remi André Løvoll <56019927+lovoll@users.noreply.github.com>
2025-08-25 15:46:47 +0200

    Fixed id, filename and bruno test name to be correct (#548)
    Fixed merge error in tokenhandling javascript

--- test/Bruno/Altinn.ResourceRegistry/TestToolsTokenGenerator.js
+++ test/Bruno/Altinn.ResourceRegistry/TestToolsTokenGenerator.js
@@ -1,39 +1,37 @@
+exports.getToken = async function (getTokenParameters) {	
+  const axios = require("axios");	
+  const btoa = require("btoa");	
 
-exports.getToken = async function (getTokenParameters) {
-  const axios = require("axios");
-  const btoa = require("btoa");
+  const tokenBaseUrl = "https://altinn-testtools-token-generator.azurewebsites.net";	
+  const basicAuthUser = bru.getEnvVar("tokenBasicAuthUser");	
+  const basicAuthPw = bru.getEnvVar("tokenBasicAuthPw");	
+  const Authorization = 'Basic ' + btoa(`${basicAuthUser}:${basicAuthPw}`);	
 
-  const tokenBaseUrl = "https://altinn-testtools-token-generator.azurewebsites.net";
-  const basicAuthUser = bru.getEnvVar("tokenBasicAuthUser");
-  const basicAuthPw = bru.getEnvVar("tokenBasicAuthPw");
-  const Authorization = 'Basic ' + btoa(`${basicAuthUser}:${basicAuthPw}`);
+  const tokenEnv = bru.getEnvVar("tokenEnv");	
+  const tokenType = getTokenParameters.auth_tokenType;	
+  const tokenScopes = getTokenParameters.auth_scopes;	
 
-  const tokenEnv = bru.getEnvVar("tokenEnv");
-  const tokenType = getTokenParameters.auth_tokenType;
-  const tokenScopes = getTokenParameters.auth_scopes;
+  let tokenUrl;	
 
-  let tokenUrl;
-	
-	if (tokenType == "Personal") {
-    const tokenUser = getTokenParameters.auth_userId;
-    const tokenParty = getTokenParameters.auth_partyId;
-    const tokenPid = getTokenParameters.auth_ssn;
+	if (tokenType == "Personal") {	
+    const tokenUser = getTokenParameters.auth_userId;	
+    const tokenParty = getTokenParameters.auth_partyId;	
+    const tokenPid = getTokenParameters.auth_ssn;	
 
-    tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${tokenScopes}&pid=${tokenPid}&userid=${tokenUser}&partyid=${tokenParty}&authLvl=3&ttl=3000`;
-  }
-  else if (tokenType == "Enterprise") {
-    const tokenOrg = getTokenParameters.auth_org;
-    const tokenOrgNo = getTokenParameters.auth_orgNo;
-    
+    tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${tokenScopes}&pid=${tokenPid}&userid=${tokenUser}&partyid=${tokenParty}&authLvl=3&ttl=3000`;	
+  }	
+  else if (tokenType == "Enterprise") {	
+    const tokenOrg = getTokenParameters.auth_org;	
+    const tokenOrgNo = getTokenParameters.auth_orgNo;	
 
-    tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${tokenScopes}&org=${tokenOrg}&orgNo=${tokenOrgNo}&ttl=30`;
-  }
 
-  else if (tokenType == "EnterpriseUser") {
-    const tokenUser = getTokenParameters.auth_userId;
-    const tokenParty = getTokenParameters.auth_partyId;
-    const tokenOrgNo = getTokenParameters.auth_orgNo;
-    const tokenUserName = getTokenParameters.auth_username;
+    tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${tokenScopes}&org=${tokenOrg}&orgNo=${tokenOrgNo}&ttl=30`;	
+  }	
+  else if (tokenType == "EnterpriseUser") {	
+    const tokenUser = getTokenParameters.auth_userId;	
+    const tokenParty = getTokenParameters.auth_partyId;	
+    const tokenOrgNo = getTokenParameters.auth_orgNo;	
+    const tokenUserName = getTokenParameters.auth_username;	
 
     tokenUrl = `${tokenBaseUrl}/api/Get${tokenType}Token?env=${tokenEnv}&scopes=${tokenScopes}&orgNo=${tokenOrgNo}&userId=${tokenUser}&partyId=${tokenParty}&userName=${tokenUserName}&ttl=30`;
   } 
@@ -60,7 +58,5 @@ exports.getToken = async function (getTokenParameters) {
     headers: { Authorization }
   });
 
-	return response.data;
-  console.log(response.data);
-  bru.setVar("bearerToken", response.data);
+	return response.data;	
 }

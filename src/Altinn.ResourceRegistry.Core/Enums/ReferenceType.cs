ecf0a8ba6a2c62656fb13e8a037ffcab42455732
Aleksander Heintz <alxandr@alxandr.me>
2025-10-15 10:56:46 +0200

    fix: swagger referencesource (#646)

--- src/Altinn.ResourceRegistry.Core/Enums/ReferenceType.cs
+++ src/Altinn.ResourceRegistry.Core/Enums/ReferenceType.cs
@@ -1,31 +1,41 @@
 ﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using Altinn.Authorization.ModelUtils;
 
-namespace Altinn.ResourceRegistry.Core.Enums
+namespace Altinn.ResourceRegistry.Core.Enums;
+
+/// <summary>
+/// Enum for reference types of resources in the resource registry
+/// </summary>
+[StringEnumConverter]
+public enum ReferenceType 
+    : int
 {
-    /// <summary>
-    /// Enum for reference types of resources in the resource registry
-    /// </summary>
-    public enum ReferenceType : int
-    {
-        [EnumMember(Value = "Default")]
-        Default = 0,
+    [EnumMember(Value = "Default")]
+    [JsonStringEnumMemberName("Default")]
+    Default = 0,
 
-        [EnumMember(Value = "Uri")]
-        Uri = 1,
-        
-        [EnumMember(Value = "DelegationSchemeId")]
-        DelegationSchemeId = 2,
+    [EnumMember(Value = "Uri")]
+    [JsonStringEnumMemberName("Uri")]
+    Uri = 1,
+    
+    [EnumMember(Value = "DelegationSchemeId")]
+    [JsonStringEnumMemberName("DelegationSchemeId")]
+    DelegationSchemeId = 2,
 
-        [EnumMember(Value = "MaskinportenScope")]
-        MaskinportenScope = 3,
-        
-        [EnumMember(Value = "ServiceCode")]
-        ServiceCode = 4,
+    [EnumMember(Value = "MaskinportenScope")]
+    [JsonStringEnumMemberName("MaskinportenScope")]
+    MaskinportenScope = 3,
+    
+    [EnumMember(Value = "ServiceCode")]
+    [JsonStringEnumMemberName("ServiceCode")]
+    ServiceCode = 4,
 
-        [EnumMember(Value = "ServiceEditionCode")]
-        ServiceEditionCode = 5,
+    [EnumMember(Value = "ServiceEditionCode")]
+    [JsonStringEnumMemberName("ServiceEditionCode")]
+    ServiceEditionCode = 5,
 
-        [EnumMember(Value = "ApplicationId")]
-        ApplicationId = 6,
-    }
+    [EnumMember(Value = "ApplicationId")]
+    [JsonStringEnumMemberName("ApplicationId")]
+    ApplicationId = 6,
 }
--- src/Altinn.ResourceRegistry.Core/Enums/ResourcePartyType.cs
+++ src/Altinn.ResourceRegistry.Core/Enums/ResourcePartyType.cs
@@ -1,27 +1,32 @@
 ﻿using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
+using Altinn.Authorization.ModelUtils;
 
-namespace Altinn.ResourceRegistry.Core.Enums
+namespace Altinn.ResourceRegistry.Core.Enums;
+
+/// <summary>
+/// Defines the type of party that a resource is targeting
+/// </summary>
+[StringEnumConverter]
+public enum ResourcePartyType
 {
-    /// <summary>
-    /// Defines the type of party that a resource is targeting
-    /// </summary>
-    [JsonConverter(typeof(JsonStringEnumConverter))]
-    public enum ResourcePartyType
-    {
-        [EnumMember(Value = "PrivatePerson")]
-        PrivatePerson = 0,
+    [EnumMember(Value = "PrivatePerson")]
+    [JsonStringEnumMemberName("PrivatePerson")]
+    PrivatePerson = 0,
 
-        [EnumMember(Value = "LegalEntityEnterprise")]
-        LegalEntityEnterprise = 1,
+    [EnumMember(Value = "LegalEntityEnterprise")]
+    [JsonStringEnumMemberName("LegalEntityEnterprise")]
+    LegalEntityEnterprise = 1,
 
-        [EnumMember(Value = "Company")]
-        Company = 2,
+    [EnumMember(Value = "Company")]
+    [JsonStringEnumMemberName("Company")]
+    Company = 2,
 
-        [EnumMember(Value = "BankruptcyEstate")]
-        BankruptcyEstate = 3,
+    [EnumMember(Value = "BankruptcyEstate")]
+    [JsonStringEnumMemberName("BankruptcyEstate")]
+    BankruptcyEstate = 3,
 
-        [EnumMember(Value = "SelfRegisteredUser")]
-        SelfRegisteredUser = 4
-    }
+    [EnumMember(Value = "SelfRegisteredUser")]
+    [JsonStringEnumMemberName("SelfRegisteredUser")]
+    SelfRegisteredUser = 4,
 }

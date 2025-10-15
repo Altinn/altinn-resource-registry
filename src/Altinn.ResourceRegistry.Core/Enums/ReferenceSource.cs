4a2b387e76b8c6d0aa7636575f3b13df10b5af6a
Aleksander Heintz <alxandr@alxandr.me>
2025-10-15 10:41:33 +0200

    fix: swagger referencesource

--- src/Altinn.ResourceRegistry.Core/Enums/ReferenceSource.cs
+++ src/Altinn.ResourceRegistry.Core/Enums/ReferenceSource.cs
@@ -1,25 +1,33 @@
 ﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using Altinn.Authorization.ModelUtils;
 
-namespace Altinn.ResourceRegistry.Core.Enums
+namespace Altinn.ResourceRegistry.Core.Enums;
+
+/// <summary>
+/// Enum for the different reference sources for resources in the resource registry
+/// </summary>
+[StringEnumConverter]
+public enum ReferenceSource 
+    : int
 {
-    /// <summary>
-    /// Enum for the different reference sources for resources in the resource registry
-    /// </summary>
-    public enum ReferenceSource : int
-    {
-        [EnumMember(Value = "Default")]    
-        Default = 0,
+    [EnumMember(Value = "Default")]
+    [JsonStringEnumMemberName("Default")]
+    Default = 0,
 
-        [EnumMember(Value = "Altinn1")]
-        Altinn1 = 1,
+    [EnumMember(Value = "Altinn1")]
+    [JsonStringEnumMemberName("Altinn1")]
+    Altinn1 = 1,
 
-        [EnumMember(Value = "Altinn2")]
-        Altinn2 = 2,
+    [EnumMember(Value = "Altinn2")]
+    [JsonStringEnumMemberName("Altinn2")]
+    Altinn2 = 2,
 
-        [EnumMember(Value = "Altinn3")]
-        Altinn3 = 3,
+    [EnumMember(Value = "Altinn3")]
+    [JsonStringEnumMemberName("Altinn3")]
+    Altinn3 = 3,
 
-        [EnumMember(Value = "ExternalPlatform")]
-        ExternalPlatform = 4
-    }
+    [EnumMember(Value = "ExternalPlatform")]
+    [JsonStringEnumMemberName("ExternalPlatform")]
+    ExternalPlatform = 4,
 }

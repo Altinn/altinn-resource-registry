18e5de2dd3222b301819ffc8c871d0a85a63474b
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2025-10-27 12:45:05 +0100

    stringenumconverter added to ResourceAccessListMode

--- src/Altinn.ResourceRegistry.Core/Enums/ResourceAccessListMode.cs
+++ src/Altinn.ResourceRegistry.Core/Enums/ResourceAccessListMode.cs
@@ -1,12 +1,21 @@
-﻿namespace Altinn.ResourceRegistry.Core.Enums
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using Altinn.Authorization.ModelUtils;
+
+namespace Altinn.ResourceRegistry.Core.Enums
 {
     /// <summary>
     /// Enum representation of the different types of ResourceAccessListModes supported by the resource registry
     /// </summary>
+    [StringEnumConverter]
     public enum ResourceAccessListMode
     {
+        [EnumMember(Value = "Disabled")]
+        [JsonStringEnumMemberName("Disabled")]
         Disabled = 0,
 
+        [EnumMember(Value = "Enabled")]
+        [JsonStringEnumMemberName("Enabled")]
         Enabled = 1,
      }
 }

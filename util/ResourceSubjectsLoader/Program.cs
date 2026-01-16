dc7319a965442be94d1264d6a6ef61a8eeec8f84
Rune T. Larsen <rune@hjemmekino.no>
2026-01-16 14:06:03 +0100

    subect

--- util/ResourceSubjectsLoader/Program.cs
+++ util/ResourceSubjectsLoader/Program.cs
@@ -6,7 +6,7 @@ using ResourceSubjectsLoader.Enums;
 
 Console.WriteLine("Hello, World!");
 
-string at22base = "https://platform.tt02.altinn.no/resourceregistry/api/v1/";
+string at22base = "https://platform.at22.altinn.cloud/resourceregistry/api/v1/";
 
 HttpClient _httpClient = new HttpClient();
 _httpClient.BaseAddress = new Uri(at22base);    
@@ -39,5 +39,19 @@ for (int i = 0; i < resourceList.Count; i++)
         await resourceRegistry.ReloadResourceSubects(resource.Identifier);
     }
 
+    if (resource.ResourceType.Equals(ResourceType.CorrespondenceService))
+    {
+        await resourceRegistry.ReloadResourceSubects(resource.Identifier);
+    }
+
+    if (resource.ResourceType.Equals(ResourceType.BrokerService))
+    {
+        await resourceRegistry.ReloadResourceSubects(resource.Identifier);
+    }
+
+    if (resource.ResourceType.Equals(ResourceType.MaskinportenSchema))
+    {
+        await resourceRegistry.ReloadResourceSubects(resource.Identifier);
+    }
 
 }
